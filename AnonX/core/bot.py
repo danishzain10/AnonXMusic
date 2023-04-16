cbeb26f78670bb90cb984e09a61de96d8258cbbf
Golu jha <121981620+golujha9936@users.noreply.github.com>
2023-04-16 06:05:26 +0530

    Update bot.py
    Signed-off-by: Golu jha <121981620+golujha9936@users.noreply.github.com>

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,7 +12,7 @@ class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "kʌτ༐ʟا͢🇽ᖾɛɛ֟፝иʌ⏤",
+            "𝗦𝗣𝗔𝗥𝗧𝗔𝗡 𝗠𝗨𝗦𝗜𝗖",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
